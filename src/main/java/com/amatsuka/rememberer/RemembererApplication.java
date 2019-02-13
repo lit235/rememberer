2777778c98488e9cc63a351cd9639dd2f1b2d9fc
amatsuka <run100500@gmail.com>
2019-02-13 15:41:21 +0300

    Init commit

---
+++ src/main/java/com/amatsuka/rememberer/RemembererApplication.java
@@ -0,0 +1,14 @@
+package com.amatsuka.rememberer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemembererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemembererApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/amatsuka/rememberer/RemembererApplicationTests.java
@@ -0,0 +1,17 @@
+package com.amatsuka.rememberer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RemembererApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
